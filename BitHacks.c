149f3a251335ee96555a81ae176e931d0e2812d0
Temitope Egunbiyi <SN032074@firstbanknigeria.com>
2025-06-02 08:32:59 +0100

    identites & operators

--- BitHacks.c
+++ BitHacks.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+// identity
+// x + ~x = -1 => ~x = x + 1
 
+// operators
+// & and
+// | or
+// ^ xor
+// ~ not (two's complement)
+// << left shift
+// >> right shit
 int main(){
 	printf("hello, world");
 }
