7ca45df025c25abee1dc5c0d26312561b6a92409
Temitope Egunbiyi <SN032074@firstbanknigeria.com>
2025-06-02 09:06:00 +0100

    set k bit to 1

--- BitHacks.c
+++ BitHacks.c
@@ -10,6 +10,22 @@
 // ~ not (two's complement)
 // << left shift
 // >> right shit
+void print_binary(int ptr);
 int main(){
-	printf("hello, world");
+	printf("hello, world\n");
+	
+	//set k bit in x to 1
+	int x=0b101001011;
+	//set 7bit to 1
+	int x_res=x | (1<<7);
+	print_binary(x_res);
+	return 0;
+}
+
+void print_binary(int ptr){
+	int size=sizeof(int)*8;
+	for(int j=size-1;j>=0;j--){
+		int mask=1<<j;
+		printf("%d",mask&ptr?1:0);
+	}
 }
